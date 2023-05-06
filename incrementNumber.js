980fd8176533c26a80e8771606a9b4a5f7002366
Jtfink <68623825+Jtfi@users.noreply.github.com>
2023-05-06 13:27:38 -0400

    Basic Increment Button and Number
    Added a button to increment a number on screen

---
+++ incrementNumber.js
@@ -0,0 +1,10 @@
+function incrementButton() {
+    var element = document.getElementById('incrementText');
+    var value = element.innerHTML;
+    
+    ++value;
+    
+    console.log(value);
+    document.getElementById('incrementText').innerHTML = value;
+    
+}
